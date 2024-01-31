f66ef3b6d3bb3d49c301223b82cbaccab05d54e0
srupat <srujanpatwardhan@gmail.com>
2024-01-31 13:56:48 +0530

    ggplot2

--- lab2.R
+++ lab2.R
@@ -1,7 +1,3 @@
-match_stat <- data.frame(name=c("Tendulkar", "Ponting", "Kallis", "Dravid", "Cook"),
-                    matches=c(200, 168, 166, 164, 161), 
-                    innings=c(329, 287, 280, 286, 291),
-                    highestscore=c(248, 257, 224, 270, 294),
-                    avg=c(53.78, 51.85, 55.37, 52.31, 45.35))
+
 
 
